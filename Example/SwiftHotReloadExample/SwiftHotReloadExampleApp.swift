7947e1da437f984944afab8cbdddacbad9be4484
banjun <banjun@gmail.com>
2023-10-26 21:07:36 +0900

    add Example new project

---
+++ Example/SwiftHotReloadExample/SwiftHotReloadExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHotReloadExampleApp.swift
+//  SwiftHotReloadExample
+//
+//  Created by BAN Jun on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHotReloadExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
